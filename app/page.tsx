7005001644f15c359f576600105bda8866e24e14
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2024-04-01 13:03:34 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,23 @@ export default function Home() {
   const [year, setYear] = useState<string>('');
   const [age, setAge] = useState<{ years: number; months: number; days: number } | null>(null);
 
+  const [errorD, setErrorD] = useState<string>("");
+  const [errorM, setErrorM] = useState<string>("");
+  const [errorY, setErrorY] = useState<string>("");
+
   const calculateAge = () => {
+
     const today = new Date();
     const birthDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
+    
+    const birthDay = parseInt(day);
+    const birthMonth = parseInt(month);
+    const birthYear = parseInt(year);
+    
+    // inputs validity
+    isNaN(birthDay) || birthDay <= 0 || birthDay > 31 ? setErrorD("Invalid day") : setErrorD("");
+    isNaN(birthMonth) || birthMonth <= 0 || birthMonth > 12 ? setErrorM("Invalid Month") : setErrorM("");
+    birthYear > today.getFullYear() || isNaN(birthYear) ? setErrorY("Invalid year") : setErrorY("");
 
     let years = today.getFullYear() - birthDate.getFullYear();
     let months = today.getMonth() - birthDate.getMonth();
@@ -27,38 +41,50 @@ export default function Home() {
       days += tempDate.getDate();
     }
 
-    setAge({ years, months, days });
+    setAge({ years, months, days })
   };
   return (
     <main className="bg-lgrey h-screen w-screen flex justify-center items-center">
       <div className="bg-white w-2/4 h-4/6 rounded-ee-[10rem] rounded-xl p-10">
       <div className="flex justify-right gap-6 px-4">
       <div className="flex flex-col">
-        <label className="font-poppins text-xs text-gray-500 font-semibold">DAY</label>
+        <label className="font-poppins text-xs text-gray-500 font-semibold opacity-80">DAY</label>
         <input 
-        className="border rounded-md h-12 w-32 outline-purple-300 cursor-pointer font-poppins text-black font-bold text-xl p-2"
+        className={`border-2 rounded-md h-12 w-32 ${errorD ? "border-red-400" : "border-2"}${errorD ? "outline-red-400" : "outline-purple-300"} cursor-pointer font-poppins text-black font-bold text-xl p-2`} 
         placeholder="DD" 
         type="number" 
         value={day} 
-        onChange={(e) => setDay(e.target.value)} />
+        onChange={(e) => setDay(e.target.value)} 
+        />
+        {
+          errorD && <p className="text-red-400">{errorD}</p>
+        }
       </div>
       <div className="flex flex-col">
-        <label className="font-poppins text-xs text-gray-500 font-semibold">MONTH</label>
+        <label className="font-poppins text-xs text-gray-500 font-semibold opacity-80">MONTH</label>
         <input 
-        className="border rounded-md h-12 w-32 outline-purple-400 cursor-pointer font-poppins text-black font-bold text-xl p-2" 
+        className={`border-2 rounded-md h-12 w-32 ${errorM ? "border-red-400" : "border-2"}${errorM ? "outline-red-400" : "outline-purple-300"} cursor-pointer font-poppins text-black font-bold text-xl p-2`} 
         type="number" 
         placeholder="MM"
         value={month} 
-        onChange={(e) => setMonth(e.target.value)} />
+        onChange={(e) => setMonth(e.target.value)}
+        />
+        {
+          errorM && <p className="text-red-400">{errorM}</p>
+        }
       </div>
       <div className="flex flex-col">
-        <label className="font-poppins text-xs text-gray-500 font-semibold">YEAR</label>
+        <label className="font-poppins text-xs text-gray-500 font-semibold opacity-80">YEAR</label>
         <input 
-        className="border rounded-md h-12 w-32 outline-purple-400 cursor-pointer font-poppins text-black font-bold text-xl p-2" 
+       className={`border-2 rounded-md h-12 w-32 ${errorY ? "border-red-400" : "border-2"}${errorY ? "outline-red-400" : "outline-purple-300"} cursor-pointer font-poppins text-black font-bold text-xl p-2`}  
         type="number" 
         placeholder="YYYY"
         value={year} 
-        onChange={(e) => setYear(e.target.value)} />
+        onChange={(e) => setYear(e.target.value)} 
+        />
+        {
+          errorY && <p className="text-red-400">{errorY}</p>
+        }
       </div>
       </div>
       <div className="flex justify-right items-center">
